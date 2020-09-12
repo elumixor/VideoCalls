d98847e717f86a8ea2059df4f74959e8ff7814ef
elumixor <vladogim97@gmail.com>
2020-09-12 18:02:06 +0200

    Update server/index.js

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+    })
+
+    socket.on('login', user => {
+        console.log('Trying to login as ' + user)
+    })
+})
+
+server.listen(3000, () => {
+    console.log('listening on localhost:3000')
+})
